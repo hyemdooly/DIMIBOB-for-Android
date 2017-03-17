9a9d720514b0638e9407e6c1d963ce43279665f8
hyemdooly <hyemdooly@gmail.com>
2017-03-17 15:05:53 +0900

    widget provider edit and error

--- app/src/main/java/kdmhs/me/hyemdooly/dimibob/WidgetProvider.java
+++ app/src/main/java/kdmhs/me/hyemdooly/dimibob/WidgetProvider.java
@@ -7,6 +7,9 @@ import android.content.Context;
 import android.content.Intent;
 import android.widget.RemoteViews;
 
+import java.io.IOException;
+import java.util.Map;
+
 /**
  * Created by songhyemin on 2017. 3. 7..
  */
@@ -18,25 +21,30 @@ public class WidgetProvider extends AppWidgetProvider {
     public void onReceive(Context context, Intent intent) {
         super.onReceive(context, intent);
 
-        //내가 작성한다~~~~~~
-        String mAction = intent.getAction();
+        ComponentName thisAppWidgetIds = new ComponentName(context.getPackageName(), getClass().getName());
+        AppWidgetManager appWidgetManager = AppWidgetManager.getInstance(context);
+        int ids[] = appWidgetManager.getAppWidgetIds(thisAppWidgetIds);
 
-        if(AppWidgetManager.ACTION_APPWIDGET_ENABLED.equals(mAction)) {
-
-        } else if(AppWidgetManager.ACTION_APPWIDGET_UPDATE.equals(mAction)) {
-            AppWidgetManager mManager = AppWidgetManager.getInstance(context);
-            initWidget(context, mManager, mManager.getAppWidgetIds(new ComponentName(context, getClass())));
+        for(int appWidgetId : ids){
+            updateWidget(context, appWidgetManager, appWidgetId);
         }
+
     }
 
     @Override
     public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
         super.onUpdate(context, appWidgetManager, appWidgetIds);
 
-        for(int i = 0; i < appWidgetIds.length; i++) {
-            final RemoteViews remoteViews = new RemoteViews(context.getPackageName(), R.layout.widget_today);
-            appWidgetManager.updateAppWidget(appWidgetIds[i], remoteViews);
+
+        for(int appWidgetId : appWidgetIds){
+
+            RemoteViews remoteViews = new RemoteViews(context.getPackageName(), R.layout.widget_today);
+            appWidgetManager.updateAppWidget(appWidgetId, remoteViews);
+            updateWidget(context, appWidgetManager, appWidgetId);
+
+
         }
+
     }
 
     @Override
@@ -55,61 +63,35 @@ public class WidgetProvider extends AppWidgetProvider {
     }
 
 
-    public void initWidget(final Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+    public void updateWidget(final Context context, AppWidgetManager appWidgetManager, int appWidgetId) {
 
-        final RemoteViews remoteViews = new RemoteViews(context.getPackageName(), R.layout.widget_today);
+        final RemoteViews updateViews = new RemoteViews(context.getPackageName(), R.layout.widget_today);
 
-        for(int appWidgetId : appWidgetIds) {
-            appWidgetManager.updateAppWidget(appWidgetId, remoteViews);
-        }
-
-        /*final HttpRequest httpRequest = new HttpRequest();
-
-        final android.os.Handler handler = new android.os.Handler(){
-            @Override
-            public void handleMessage(Message msg) {
-                Bundle bundle = msg.getData();
-
-                remoteViews.setTextViewText(R.id.breakfast, bundle.getString("breakfast"));
-                remoteViews.setTextViewText(R.id.lunch, bundle.getString("lunch"));
-                remoteViews.setTextViewText(R.id.dinner, bundle.getString("dinner"));
-                remoteViews.setTextViewText(R.id.snack, bundle.getString("snack"));
-
-            }
-        };
-
-
-        new Thread(new Runnable() {
+        Thread t = new Thread(new Runnable() {
             @Override
             public void run() {
-                try{
-                    Map<String, String> response = httpRequest.getRequest();
+                HttpRequest httpRequest = new HttpRequest();
+                Map<String, String> response = null;
 
-                    Message msg = handler.obtainMessage();
-                    Bundle bundle = new Bundle();
+                try {
+                    response = httpRequest.getRequest();
+                    updateViews.setTextViewText(R.id.breakfast, response.get("breakfast").toString());
+                    updateViews.setTextViewText(R.id.dinner, response.get("dinner").toString());
+                    updateViews.setTextViewText(R.id.lunch, response.get("lunch").toString());
+                    updateViews.setTextViewText(R.id.snack, response.get("snack").toString());
 
-                    bundle.putCharSequence("breakfast",response.get("breakfast").toString());
-                    bundle.putCharSequence("lunch",response.get("lunch").toString());
-                    bundle.putCharSequence("dinner",response.get("dinner").toString());
-                    bundle.putCharSequence("snack",response.get("snack").toString());
-
-                    msg.setData(bundle);
-
-                    handler.sendMessage(msg);
-
-                } catch(Exception e){
+                } catch (IOException e) {
                     e.printStackTrace();
                 }
 
-
-
-
             }
-        }).start();*/
+        });
 
+        t.start();
+
+        appWidgetManager.updateAppWidget(appWidgetId, updateViews);
 
 
     }
-
 
 }
